eccff1bafb8b6e1c0a210519d2d42785dfa73058
20101906isaac <20101906isaac@gmail.com>
2026-04-20 15:13:40 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Este script puede usarse para detectar el día de la semana
+// y resaltar el botón de la reunión de hoy.
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Web de Reuniones Boulevares lista.");
+});
